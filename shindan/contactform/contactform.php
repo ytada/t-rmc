384f45ede06a98f8f8fa450cc407d419983b98b0
Yukio TADA <yukio.tada@gmail.com>
2019-04-30 15:08:13 +0900

    コンタクトフォームの送信先メールアドレスを修正

--- shindan/contactform/contactform.php
+++ shindan/contactform/contactform.php
@@ -16,7 +16,7 @@
   $contactform->ajax = true;
 
   // Replace with your real receiving email address
-  $contactform->to = 'tada@t-rmc.jp';
+  $contactform->to = 'shindan@yukio-tada.com';
   $contactform->from_name = $_POST['name'];
   $contactform->from_email = $_POST['email'];
   $contactform->subject = $_POST['subject'];
